e9ec89c04c721c8ccdc9434c2fa09eae815353fe
Steven <stevenbellamy@hotmail.com>
2016-08-01 15:23:23 +1200

    Starting adding snake collision

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -17,7 +17,7 @@ class Game
 			mPlayer.setRadius(10.f);
 			mPlayer.setPosition(100.f, 100.f);
 			mPlayer.setFillColor(sf::Color::Yellow);
-
+			/*
 			sf::CircleShape s1;
 			s1.setRadius(10.f);
 			s1.setPosition(80.f, 100.f);
@@ -36,6 +36,8 @@ class Game
 			snake.push_back(s1);
 			snake.push_back(s2);
 			snake.push_back(s3);
+			*/
+			snake.push_back(mPlayer);
 		}
 		void Game::run()
 		{
@@ -98,10 +100,13 @@ class Game
 				foodPiece.setFillColor(sf::Color::Green);
 				food.push_back(foodPiece);
 			}
-			//Update the snakes body pathing every second
+			//Update the snake every 10th of a second
 			if (clock.getElapsedTime().asSeconds() > 0.1f)
 			{
+				//Restart the clock
 				clock.restart();
+
+				//Update the snakes body segement locations
 				for (int i = snake.size()-1; i > 0; i--)
 				{
 					if(i != 0)
@@ -109,7 +114,10 @@ class Game
 						snake[i].setPosition(snake[i - 1].getPosition());
 					}
 				}
+				//Get the snakes head location
 				sf::Vector2f snakePos = snake[0].getPosition();
+
+				//Find the snakes direction and keep the momentum going
 				if (direction == 1)
 				{
 					snakePos.x -= 20.f;
@@ -126,12 +134,29 @@ class Game
 				{
 					snakePos.y += 20.f;
 				}
+				//Changing the snakes head location
 				snake[0].setPosition(snakePos);
+
+				//Check if the snake is colliding with itself
+				for (int i = 1; i < snake.size(); i++)
+				{
+					if (snake[i].getPosition() == snakePos)
+					{
+						std::cout << "COLIDE" << std::endl;
+					}
+				}
+				//Check if the snake is eating food
 				for (int i = 0; i < food.size(); i++)
 				{
 					if (food[i].getPosition().x == snakePos.x && food[i].getPosition().y == snakePos.y)
 					{
 						food.erase(food.begin() + 0);
+
+						sf::CircleShape part;
+						part.setRadius(10.f);
+						part.setPosition(-10000.f, -10000.f);
+						part.setFillColor(sf::Color::Cyan);
+						snake.push_back(part);
 					}
 				}
 			}
